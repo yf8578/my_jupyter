24f8b6ff4c6e9ea34f064a3c60fdeb64544a2292
zhangyifan1 <zhangyifan1@genomics.cn>
2023-02-13 17:44:38 +0800

    20230213

--- 20230206_tpm_count/get_gene_summary.py
+++ 20230206_tpm_count/get_gene_summary.py
@@ -6,18 +6,19 @@ import sys
 # 传入参数input1<stringtie/.gene_abund.txt>,inpu2<result_summary/.gene_count.txt>
 # # output1< >,output2
 # ref = sys.argv[1]
-input1 = sys.argv[2]
-input2 = sys.argv[3]
-output1 = sys.argv[4]
-output2 = sys.argv[5]
-# ref = "D:\\githubku\\my_jupyter\\20230206_tpm_count\\merge1.txt"
+ref = "D:\\githubku\\my_jupyter\\20230206_tpm_count\\merge1.txt"
+input1 = sys.argv[1]
+input2 = sys.argv[2]
+# output1 = sys.argv[3]
+# output2 = sys.argv[4]
 # # 输入
 # input1 = "D:\\githubku\\my_jupyter\\20230206_tpm_count\\SGI75460067.gene_abund.txt"
 # input2 = "D:\\githubku\\my_jupyter\\20230206_tpm_count\\SGI75460067.gene_count.txt"
-
+# path = "D:\\githubku\\my_jupyter\\20230206_tpm_count\\"
+# fileid = "SGI75460067"
 # 输出
-path=sys.argv[6]
-fileid=sys.argv[7]
+path=sys.argv[3]
+fileid=sys.argv[4]
 
 # output1 = "D:\\githubku\\my_jupyter\\20230206_tpm_count\\SGI75460067.type_TPM.txt"
 # output2 = "D:\\githubku\\my_jupyter\\20230206_tpm_count\\SGI75460067.type_count.txt"
@@ -25,7 +26,8 @@ fileid=sys.argv[7]
 # output4 = "D:\\githubku\\my_jupyter\\20230206_tpm_count\\SGI75460067.name_tpm.txt"
 # output5 = "D:\\githubku\\my_jupyter\\20230206_tpm_count\\SGI75460067.name_cpm.txt"
 
-
+ref1_file = pd.read_csv(ref, sep='\t', engine='python')
+Name_type = ref1_file.drop_duplicates(subset=['Gene Name'], keep='first')
 # 以csv格式读入ref文件，tab为分隔符,列名分别设置为id，Gene Name，type，
 ref1_file = pd.read_csv(ref, sep='\t', engine='python')
 # 获取ref1_file的type列，去重,保存为dataframe类型,赋值给变量type_tpm,添加四列TPM>0,TPM>1,TPM>5,TPM>10
@@ -60,7 +62,10 @@ Gene_ID_type = Gene_ID_type.drop_duplicates(['Gene ID'])
 # 获取ref1_file中Gene ID以unassigned开头的数据,保存为变量unassigned_type
 unassigned_type = ref1_file[ref1_file['Gene ID'].str.contains(
     'unassigned_transcript_', na=False)]
-#*******************PART1 START*********************#
+# 获取ref1_file中gene name和gene id都存在的数据,保存为part3
+part3 = ref1_file[ref1_file['Gene Name'].notnull() &
+                  ref1_file['Gene ID'].notnull()]
+## ***********PART1 START***********##
 
 # 以csv格式读入input1文件，以tab为分隔符，不设置列名，保留第二列和最后一列数据
 input1_file = pd.read_csv(input1, sep='\t', usecols=[
@@ -83,7 +88,8 @@ input1_other = input1_other[~input1_other['Gene ID'].str.startswith('unknown')]
 
 # 处理ENST开头的数据
 # 以input1_ENST的Gene ID和ref1_file的ENST为键，input1_ENST为主表，合并input1_ENST和ref1_file，保存为变量ENST_tpm，并输出
-ENST_tpm = pd.merge(input1_ENST, ENST_type, left_on='Gene ID', right_on='ENST', how='left')
+ENST_tpm = pd.merge(input1_ENST, ENST_type,
+                    left_on='Gene ID', right_on='ENST', how='left')
 # 获取ENST_tpm中Gene type列为nan的数据m_nan
 ENST_tpm_nan = ENST_tpm[ENST_tpm['Gene type'].isnull()]
 # ENST_tpm_nan保留Gene ID、Gene Name和TPM列
@@ -94,7 +100,8 @@ ENST_tpm = ENST_tpm[ENST_tpm['Gene type'].notnull()]
 ENST_tpm_df = type_tpm
 # 遍历ENST_tpm_df索引，统计type_tpmTPM>0,TPM>1,TPM>5,TPM>10的数量
 for i in ENST_tpm_df.index:
-    ENST_tpm_type = ENST_tpm[ENST_tpm['Gene type'] == ENST_tpm_df.loc[i, 'Gene type']]
+    ENST_tpm_type = ENST_tpm[ENST_tpm['Gene type']
+                             == ENST_tpm_df.loc[i, 'Gene type']]
     for j in ENST_tpm_type.index:
         if ENST_tpm_type.loc[j, 'TPM'] > 0:
             ENST_tpm_df.loc[i, 'TPM>0'] += 1
@@ -106,7 +113,8 @@ for i in ENST_tpm_df.index:
             ENST_tpm_df.loc[i, 'TPM>10'] += 1
 
 # 以input1_number的Gene ID和ref1_file的Gene ID为键，input1_number为主表，合并input1_number和ref1_file，保存为变量Gene_ID_tpm，并输出
-Gene_ID_tpm = pd.merge(input1_number, Gene_ID_type, left_on='Gene ID', right_on='Gene ID', how='left')
+Gene_ID_tpm = pd.merge(input1_number, Gene_ID_type,
+                       left_on='Gene ID', right_on='Gene ID', how='left')
 
 # 获得Gene_ID_tpm中Gene type为nan的数据，保存为变量Gene_ID_tpm_nan
 Gene_ID_tpm_nan = Gene_ID_tpm[Gene_ID_tpm['Gene type'].isnull()]
@@ -115,16 +123,19 @@ Gene_ID_tpm_nan = Gene_ID_tpm_nan[['Gene ID', 'Gene Name', 'TPM']]
 # 删除Gene_ID_tpm中Gene type为nan的数据
 Gene_ID_tpm = Gene_ID_tpm[Gene_ID_tpm['Gene type'].notnull()]
 
-######只能通过gene name来进行比对的数据，还有一部分不能通过gene name查到
+# 只能通过gene name来进行比对的数据，还有一部分不能通过gene name查到
 # 合并Gene_ID_tpm_nan、ENST_tpm_nan、input1_other、input1_unknown
-input1_byname = pd.concat([Gene_ID_tpm_nan, ENST_tpm_nan, input1_other, input1_unknown])
+input1_byname = pd.concat(
+    [Gene_ID_tpm_nan, ENST_tpm_nan, input1_other, input1_unknown])
 # 以input1_byname的Gene Name和ref1_file的Gene Name为键，input1_byname为主表
-Name_tpm = pd.merge(input1_byname, Name_type, left_on='Gene Name', right_on='Gene Name', how='left')
+Name_tpm = pd.merge(input1_byname, Name_type,
+                    left_on='Gene Name', right_on='Gene Name', how='left')
 # 遍历NAME_tpm_df索引，统计NAME_tpm_dfTPM>0,TPM>1,TPM>5,TPM>10的数量
 NAME_tpm_df = type_tpm
 for i in NAME_tpm_df.index:
     # 获取Name_tpm中Gene type为NAME_tpm_df中Gene type的数据
-    Name_tpm_type = Name_tpm[Name_tpm['Gene type'] == NAME_tpm_df.loc[i, 'Gene type']]
+    Name_tpm_type = Name_tpm[Name_tpm['Gene type']
+                             == NAME_tpm_df.loc[i, 'Gene type']]
     # 遍历Name_tpm_type索引，统计TPM>0,TPM>1,TPM>5,TPM>10的数量
     for j in Name_tpm_type.index:
         if Name_tpm_type.loc[j, 'TPM'] > 0:
@@ -139,7 +150,8 @@ for i in NAME_tpm_df.index:
 # 遍历ID_tpm_df索引，统计ID_tpm_dfTPM>0,TPM>1,TPM>5,TPM>10的数量
 ID_tpm_df = type_tpm
 for i in ID_tpm_df.index:
-    ID_tpm_type = Gene_ID_tpm[Gene_ID_tpm['Gene type'] == ID_tpm_df.loc[i, 'Gene type']]
+    ID_tpm_type = Gene_ID_tpm[Gene_ID_tpm['Gene type']
+                              == ID_tpm_df.loc[i, 'Gene type']]
     for j in ID_tpm_type.index:
         if ID_tpm_type.loc[j, 'TPM'] > 0:
             ID_tpm_df.loc[i, 'TPM>0'] += 1
@@ -155,8 +167,6 @@ for i in ID_tpm_df.index:
 
 type_tpm.to_csv(path+'/'+fileid+"TPM_count.txt", index=False)
 print('finished'+str(path+'/'+fileid+"TPM_count.txt"))
-# *********************************PART1 END*********************************#
-
 # *********************************PART2 START*********************************#
 # 以csv格式读入input2文件，以,分隔，不设置列名，删除第一行
 input2_file = pd.read_csv(input2, sep=',', header=None, skiprows=1,engine='python')
@@ -166,9 +176,28 @@ input2_file1 = input2_file[0].str.split('|', expand=True)
 input2_file = pd.concat([input2_file1, input2_file[1]], axis=1)
 input2_file.columns = ['Gene ID', 'Gene Name', 'count']
 input2_file = input2_file.reset_index(drop=True)
-
+input2_file_part3 = input2_file
+# 获得input2_file_part3中Gene name为nan的数据
+input2_file_part3_nan = input2_file_part3[input2_file_part3['Gene Name'].isnull(
+)]
+# 根据gene id在unassigned_type中查找gene name
+input2_file_part3_nan = pd.merge(
+    input2_file_part3_nan, part3, left_on='Gene ID', right_on='Gene ID', how='left')
+# 保留input2_file_part3_nan的Gene ID、Gene Name和count列
+input2_file_part3_nan = input2_file_part3_nan[[
+    'Gene ID', 'Gene Name_y', 'count']]
+# 将input2_file_part3_nan的Gene Name_y改为Gene Name
+input2_file_part3_nan.columns = ['Gene ID', 'Gene Name', 'count']
+# 删除input2_file_part3中Gene name为nan的数据
+input2_file_part3 = input2_file_part3[input2_file_part3['Gene Name'].notnull()]
+# 合并input2_file_part3和input2_file_part3_nan
+input2_file_part3 = pd.concat([input2_file_part3, input2_file_part3_nan])
+# 统计input2_file_part3中Gene Name对应的count
+input2_file_part3_final = input2_file_part3.groupby('Gene Name').sum()
+# *********************************PART2 START*********************************#
 # 以csv格式读入input2文件，以,分隔，不设置列名，删除第一行
-input2_file = pd.read_csv(input2, sep=',', header=None, skiprows=1,engine='python')
+input2_file = pd.read_csv(input2, sep=',', header=None,
+                          skiprows=1, engine='python')
 # 第一列按照|分隔，保存为变量input2_file1
 input2_file1 = input2_file[0].str.split('|', expand=True)
 # 将input2_file1和input2_file合并，保存为变量input2_file
@@ -181,37 +210,45 @@ input2_ENST = input2_file[input2_file['Gene ID'].str.contains('ENST')]
 # 获取input2_file中Gene ID中纯数字的数据
 input2_ID = input2_file[input2_file['Gene ID'].str.contains('^[0-9]+$')]
 # 获得input2_file中Gene ID包含unassigned的数据
-input2_unassigned = input2_file[input2_file['Gene ID'].str.contains('unassigned')]
+input2_unassigned = input2_file[input2_file['Gene ID'].str.contains(
+    'unassigned')]
 # 获得input2_file中Gene ID不是纯数字，也不是以ENST开头的数据
 input2_other = input2_file[~input2_file['Gene ID'].str.contains('^[0-9]+$')]
 input2_other = input2_other[~input2_other['Gene ID'].str.contains('ENST')]
-input2_other = input2_other[~input2_other['Gene ID'].str.contains('unassigned')]
+input2_other = input2_other[~input2_other['Gene ID'].str.contains(
+    'unassigned')]
 
 # 将input2_ENST和ENST_type合并，以Gene ID为键，以input2_ENST为主表，保存为变量input2_ENST_type
-input2_ENST_type = pd.merge(input2_ENST, ENST_type, right_on='ENST', left_on='Gene ID', how='left')
+input2_ENST_type = pd.merge(input2_ENST, ENST_type,
+                            right_on='ENST', left_on='Gene ID', how='left')
 # 将input2_ID和ID_type合并，以Gene ID为键，保存为变量input2_ID_type
 input2_ID_type = pd.merge(input2_ID, Gene_ID_type, on='Gene ID', how='left')
 # 将input2_unassigned和unassigned_type合并，以Gene ID为键，保存为变量input2_unassigned_type
-input2_unassigned_type = pd.merge(input2_unassigned, unassigned_type, on='Gene ID', how='left')
+input2_unassigned_type = pd.merge(
+    input2_unassigned, unassigned_type, on='Gene ID', how='left')
 
 # 将input2_ENST_type中Gene type为nan的数据
 input2_ENST_type_nan = input2_ENST_type[input2_ENST_type['Gene type'].isnull()]
 # 获取input2_ID_type中Gene type为nan的数据
 input2_ID_type_nan = input2_ID_type[input2_ID_type['Gene type'].isnull()]
 # 将input2_ID_type_nan前三列和input2_other合并
-input2_other = pd.concat([input2_ID_type_nan.iloc[:, :3], input2_other], axis=0)
+input2_other = pd.concat(
+    [input2_ID_type_nan.iloc[:, :3], input2_other], axis=0)
 # 将input2_other和Name_type合并，以Gene Name为键，保存为变量input2_other_type
-input2_other_type = pd.merge(input2_other, Name_type, on='Gene Name', how='left')
+input2_other_type = pd.merge(
+    input2_other, Name_type, on='Gene Name', how='left')
 
 # 统计input2_ENST_type、input2_ID_type、input2_unassigned_type、input2_other_type中每个Gene type对应的count
 input2_ENST_type_count = input2_ENST_type.groupby('Gene type')['count'].sum()
 input2_ID_type_count = input2_ID_type.groupby('Gene type')['count'].sum()
-input2_unassigned_type_count = input2_unassigned_type.groupby('Gene type')['count'].sum()
+input2_unassigned_type_count = input2_unassigned_type.groupby('Gene type')[
+    'count'].sum()
 input2_other_type_count = input2_other_type.groupby('Gene type')['count'].sum()
 
 # 将input2_ENST_type_count、input2_ID_type_count、input2_unassigned_type_count、input2_other_type_count合并，以Gene
 # type为键，保存为变量input2_type_count
-input2_type_count = pd.concat([input2_ENST_type_count, input2_ID_type_count, input2_unassigned_type_count, input2_other_type_count], axis=0)
+input2_type_count = pd.concat([input2_ENST_type_count, input2_ID_type_count,
+                              input2_unassigned_type_count, input2_other_type_count], axis=0)
 input2_type_count = input2_type_count.reset_index()
 input2_type_count.columns = ['Gene type', 'count']
 input2_type_count = input2_type_count.groupby('Gene type')['count'].sum()
@@ -220,30 +257,29 @@ input2_type_count = input2_type_count.reset_index()
 # print('finished'+str(output2))
 input2_type_count.to_csv(path+'/'+fileid+"RNA_type.txt", index=False)
 print('finished'+str(path+'/'+fileid+"RNA_type.txt"))
-# *********************************PART2 END*********************************#
-
+################################# PART2 END#################################
 # *********************************PART3 START*********************************#
-#统计input2_file中相同Gene Name对应的count，将count从大到小排列并输出
-input2_file_count = input2_file.groupby('Gene Name')['count'].sum()
+# 统计input2_file中相同Gene Name对应的count，将count从大到小排列并输出
+input2_file_count = input2_file_part3_final
 input2_file_count = input2_file_count.reset_index()
 input2_file_count = input2_file_count.sort_values(by='count', ascending=False)
 # input2_file_count.to_csv(output3, index=False,header=False)
+# input2_file_count.to_csv(path+'/'+fileid+"Name_count.txt", index=False,header=False)
+# 计算input2_file_count中每个Gene Name对应的count的cpm值
+input2_file_count['cpm'] = input2_file_count['count'] / \
+    input2_file_count['count'].sum() * 1000000
 
-input2_file_count.to_csv(path+'/'+fileid+"Name_count.txt", index=False,header=False)
 
-#计算input2_file中每个Gene Name count的cpm值
-input2_file['cpm'] = input2_file['count'] / input2_file['count'].sum() * 1000000
-#统计input2_file中相同Gene Name对应的cpm，将cpm从大到小排列并输出
-input2_file_cpm = input2_file.groupby('Gene Name')['cpm'].sum()
-input2_file_cpm = input2_file_cpm.reset_index()
-input2_file_cpm = input2_file_cpm.sort_values(by='cpm', ascending=False)
-# input2_file_cpm.to_csv(output3+'cpm.txt', index=False,header=False)
-input2_file_cpm.to_csv(path+'/'+fileid+"Name_cpm.txt", index=False,header=False)
-
-#统计input1_file中相同Gene Name对应的TPM，将TPM从大到小排列并输出
+# 统计input1_file中相同Gene Name对应的TPM，将TPM从大到小排列并输出
 input1_file_TPM = input1_file.groupby('Gene Name')['TPM'].sum()
 input1_file_TPM = input1_file_TPM.reset_index()
-input1_file_TPM = input1_file_TPM.sort_values(by='TPM', ascending=False)
+# input1_file_TPM与input2_file_count合并，以Gene Name为键
+input1_file_TPM_cpm = pd.merge(
+    input1_file_TPM, input2_file_count, on='Gene Name', how='left')
+# input1_file_TPM = input1_file_TPM.sort_values(by='TPM', ascending=False)
 # input1_file_TPM.to_csv(output4, index=False,header=False)
-input1_file_TPM.to_csv(path+'/'+fileid+"Name_tPM.txt", index=False,header=False)
-# *********************************PART3 END*********************************#
+# input1_file_TPM.to_csv(path+'/'+fileid+"Name_tPM.txt", index=False,header=False)
+input1_file_TPM_cpm.to_csv(
+    path+'/'+fileid+"Name_tPM_cpm.txt", index=False)
+print('finished'+str(path+'/'+fileid+"Name_tPM_cpm.txt"))
+################################# PART3 END#################################
